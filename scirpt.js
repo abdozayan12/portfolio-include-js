c9f134af581ebc319daf7b9322bee526bf5e48b1
github_username <email_address>
2023-05-15 17:03:22 +0300

    update

--- scirpt.js
+++ scirpt.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".hamburger")
+const hamburger = document.querySelector(".humburger")
 const navMenu = document.querySelector(".nav-menu")
 
 hamburger.addEventListener("click",() => {
