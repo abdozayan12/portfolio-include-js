1de106675b451ec3943a246f1b413cbb6d802f55
github_username <email_address>
2023-05-15 17:55:34 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,11 +1,14 @@
-const humburger = document.querySelector(".humburger");
-const navMenu = document.querySelector(".nav-menu");
+const humburger = document.querySelector('.humburger');
+const navMenu = document.querySelector('.nav-menu');
 
-humburger.addEventListener("click",() => {
-  humburger.classList.toggle("active");
-  navMenu.classList.toggle("active");})
+humburger.addEventListener('click',() => 
+{
+  humburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+})
 
-document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
-  humburger.classList.remove("active");
-  navMenu.classList.remove("active");
+document.querySelectorAll('.nav-links').forEach(n => n.addEventListener('click', () => 
+{
+  humburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }))
