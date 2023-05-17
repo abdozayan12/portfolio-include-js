e152db17667d64b071e829a32e424c4df7d8199b
github_username <email_address>
2023-05-17 14:21:13 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -56,3 +56,17 @@ document.querySelectorAll('.nav-links').forEach((n) => n.addEventListener('click
   humburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+// validate Email //
+
+const form = document.querySelector('.form1');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const emailel = document.getElementById('email').value;
+  const valid = document.querySelector('.invalid');
+  if (emailel !== emailel.toLowerCase()) {
+    valid.innerText = 'please enter a correct email address format';
+  } else {
+    form.submit();
+  }
+});
