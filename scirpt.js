d23b8a98dd3ad9876acf1864ed4c72e13b32d2cb
github_username <email_address>
2023-05-18 16:51:55 +0300

    pre end

--- scirpt.js
+++ scirpt.js
@@ -16,14 +16,6 @@ document.querySelectorAll('.nav-links').forEach((n) => n.addEventListener('click
 const form = document.querySelector('.form1');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const fd = new FormData(form);
-  for (item of fd){
-    console.log(item);
-  }
-  
-  // const obj = object.fromEntries(fd);
-  // const json = JSON.stringify(obj);
-  // localStorage.setItem('form', json);
 
   const emailel = document.getElementById('email').value;
   const valid = document.querySelector('.invalid');
@@ -40,64 +32,18 @@ const Name = document.getElementById('name');
 const emailEl = document.getElementById('email');
 const Text = document.getElementById('textArea');
 
-
 form.addEventListener('click', () => {
   const formData = {
-    name: document.getElementById('name').value,
+    Name: document.getElementById('name').value,
     emailEl: document.getElementById('email').value,
     Text: document.getElementById('textArea').value,
   };
   localStorage.setItem('formData', JSON.stringify(formData));
 });
+// window.addEventListener('form', (e) => {
+//   document.querySelector('.my-key').textContent = e.key;
+//   document.querySelector('.my-old').textContent = e.oldValue;
+//   document.querySelector('.my-new').textContent = e.newValue;
+//   document.querySelector('.my-storage').textContent = JSON.stringify(e.storageArea);
+// });
 
-// function save () {
-//   var data;
-//   if (localstorage.getItem('data') == null){
-//     data = [];
-//   }
-//   else {
-//     data = JSON.parse(localStorage.getItem("data"));
-//   }
-
-// }
-
-// To Be Used  if one form input change // 
-window.addEventListener('storage', function(e) {
-  document.querySelector('.my-key').textContent = e.key;
-  document.querySelector('.my-old').textContent = e.oldValue;
-  document.querySelector('.my-new').textContent = e.newValue;
-  document.querySelector('.my-url').textContent = e.url;
-  document.querySelector('.my-storage').textContent = JSON.stringify(e.storageArea);
-});
-
-
-// check if exist get else update //
-if (!localStorage.getItem("bgcolor")) {
-  populateStorage();
-} else {
-  setStyles();
-}
-
-// example //
-function setStyles() {
-  const currentColor = localStorage.getItem("bgcolor");
-  const currentFont = localStorage.getItem("font");
-  const currentImage = localStorage.getItem("image");
-
-  document.getElementById("bgcolor").value = currentColor;
-  document.getElementById("font").value = currentFont;
-  document.getElementById("image").value = currentImage;
-
-  htmlElem.style.backgroundColor = `#${currentColor}`;
-  pElem.style.fontFamily = currentFont;
-  imgElem.setAttribute("src", currentImage);
-}
-
-// set value //
-function populateStorage() {
-  localStorage.setItem("bgcolor", document.getElementById("bgcolor").value);
-  localStorage.setItem("font", document.getElementById("font").value);
-  localStorage.setItem("image", document.getElementById("image").value);
-
-  setStyles();
-}
